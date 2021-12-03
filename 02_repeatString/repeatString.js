299677668ae66c8128edf7897c6df5ee31c02645
Laszlo Kiss <pinewood82@gmail.com>
2021-12-03 15:23:23 +0100

    Add a conditional and a loop to function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let res = "";
+    for (let i = 0; i < num; i++) {
+        res += text;
+    }
+    return res;
 };
 
 // Do not edit below this line
