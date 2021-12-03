17b671265a3aa151482d9b63acc8ff6fb40974d8
Laszlo Kiss <pinewood82@gmail.com>
2021-12-03 16:08:09 +0100

    Use named and rest parameters in function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...restParam) {
+  for (let item of restParam) {
+      let index = array.indexOf(item);
+      if (index < 0) {
+          continue;
+      }
+      array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
