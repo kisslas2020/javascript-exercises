b954957a8caa0ec4a90b9812bb4b8ef4180c7f5a
Laszlo Kiss <pinewood82@gmail.com>
2021-12-03 15:34:02 +0100

    Skip the string into an array and build another by looping through the array

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
-
+  let array = str.split("");
+  const N = array.length;
+  let res = "";
+  for (let i = N - 1; i >= 0; i--) {
+    res += array[i];
+  }
+  return res;
 };
 
 // Do not edit below this line
