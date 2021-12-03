83d9ffb0991dda5409f50130d37aa28a328ddbf4
Laszlo Kiss <pinewood82@gmail.com>
2021-12-03 16:22:58 +0100

    Validate arguments and make the sum by a loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,26 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    if (!isInteger(first) || !isInteger(last)) {
+        return "ERROR";
+    }
+    if (first < 0) {
+        return "ERROR";
+    }
+    if (first > last) {
+        let temp = first;
+        first = last;
+        last = temp;
+    }
 
+    let sum = 0;
+    for (let i = first; i <= last; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+function isInteger(num) {
+    return typeof(num) === "number" && Math.round(num) === num;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
