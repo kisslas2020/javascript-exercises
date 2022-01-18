0093a87764b5d62aaf5530959173473632e5d13c
Laszlo Kiss <pinewood82@gmail.com>
2022-01-18 10:50:11 +0100

    Add function using sort()

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((a, b) => {
+        let aStart = a.yearOfBirth;
+        let bStart = b.yearOfBirth;
+        let aEnd = a.yearOfDeath === undefined ? new Date().getFullYear() : a.yearOfDeath;
+        let bEnd = b.yearOfDeath === undefined ? new Date().getFullYear() : b.yearOfDeath;
+        let aAge = aEnd - aStart;
+        let bAge = bEnd - bStart;
+        return bAge - aAge;
+    })[0];
 };
 
 // Do not edit below this line
