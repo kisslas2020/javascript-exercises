626c1aa1593a6783fbe217ed4b6e2321209259fb
Laszlo Kiss <pinewood82@gmail.com>
2022-01-18 10:08:30 +0100

    Add function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    text = text.replaceAll(/\W/g, '');
+    let newText = '';
+    for (let i = text.length - 1; i >= 0; i--) {
+        newText += text.charAt(i);
+    }
+    return newText.toLowerCase() === text.toLowerCase();
 };
 
 // Do not edit below this line
